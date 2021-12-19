b7542e20d791f48f693aac5fc0da4cf0ecdd7923
DarkNormal <mark.lordan@me.com>
2021-12-19 10:27:15 +0000

    Day 23 Player logic - Turtle crossing game

--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -8,7 +8,21 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player1 = Player()
+
+screen.listen()
+screen.onkeypress(key="Up", fun=player1.move)
+
+
+def next_level():
+    pass
+    # TODO speed up cars
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if player1.is_finished():
+        next_level()
--- TurtleCrossingGame/player.py
+++ TurtleCrossingGame/player.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.left(90)
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def is_finished(self):
+        return self.ycor() >= FINISH_LINE_Y
