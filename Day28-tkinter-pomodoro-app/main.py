ac7e792438439deeade1c41e60c8b581fdff4f41
DarkNormal <mark.lordan@me.com>
2022-04-01 08:49:02 +0100

    Day28 tkinter Pomodoro app pt2

--- Day28-tkinter-pomodoro-app/main.py
+++ Day28-tkinter-pomodoro-app/main.py
@@ -21,10 +21,22 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=50, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(row=0, column=1)
+
 tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width="200", height="224", bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_btn = Button(text="Start", highlightthickness=0)
+start_btn.grid(row=2, column=0)
+
+reset_btn = Button(text="Reset", highlightthickness=0)
+reset_btn.grid(row=2, column=2)
+
+checkmark_label = Label(text="✔", fg=GREEN)
+checkmark_label.grid(row=3, column=1)
 
 window.mainloop()
