0a9bb509ba7d5c6ef732a3041a08f448ca2fc39e
DarkNormal <mark.lordan@me.com>
2022-04-08 00:13:59 +0100

    Tkinter countdown update Canvas UI

--- Day28-tkinter-pomodoro-app/main.py
+++ Day28-tkinter-pomodoro-app/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +11,24 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+def start_timer():
+    countdown_timer(300)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def countdown_timer(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown_timer, count -1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,10 +42,10 @@ timer_label.grid(row=0, column=1)
 tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width="200", height="224", bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_btn = Button(text="Start", highlightthickness=0)
+start_btn = Button(text="Start", highlightthickness=0, command=start_timer)
 start_btn.grid(row=2, column=0)
 
 reset_btn = Button(text="Reset", highlightthickness=0)
