616e7ba0119925559e7d8a3003f34eb278fbf549
DarkNormal <mark.lordan@me.com>
2022-04-12 10:48:05 +0100

    Day 29 password manager save to file

--- Day29-tkinter-password-manager-app/main.py
+++ Day29-tkinter-password-manager-app/main.py
@@ -1,14 +1,27 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save_password():
+    website = website_input.get()
+    user = username_input.get()
+    password = password_input.get()
+
+    with open("data.txt", "a") as pw_file:
+        pw_file.write(f"{website} | {user} | {password}\n")
+
+    website_input.delete(0, END)
+    password_input.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 lock_img = PhotoImage(file="logo.png")
 canvas = Canvas(width="200", height="200")
@@ -19,12 +32,14 @@ website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
 website_input = Entry(width=35)
+website_input.focus()
 website_input.grid(row=1, column=1, columnspan=2, sticky="ew")
 
 username_label = Label(text="Email/Username:")
 username_label.grid(row=2, column=0)
 
 username_input = Entry(width=35)
+username_input.insert(0, "user@example.com")
 username_input.grid(row=2, column=1, columnspan=2, sticky="ew")
 
 password_label = Label(text="Password:")
@@ -36,7 +51,7 @@ password_input.grid(row=3, column=1, sticky="w")
 generate_button = Button(text="Generate Password")
 generate_button.grid(row=3, column=2, sticky="ew")
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="ew")
 
 window.mainloop()
